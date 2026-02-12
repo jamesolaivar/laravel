c0b4b46f7e38357e1b88f8450d0d069ca55e6eff
James Olaivar <olaivar.jamespatrick@gmail.com>
2026-02-12 19:33:58 +0800

    Added CRUD Operations

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+        'name' => 'required',
+        'price' => 'required|integer',
+        'stock' => 'required|integer',
+        ]);
+
+        return Product::create($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(['message' => 'Deleted successfully']);
+    }
+}
